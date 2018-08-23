ce3e0f5f921ae165d242c8fb317efd08d768e501
yzh <you_zhihong@aliyun.com>
2018-08-23 14:43:31 +0800

    遇到 5xx 错误 尝试重新下载

--- main.py
+++ main.py
@@ -8,5 +8,15 @@
 
 import urllib
 
-def download(url):
-    return urllib.request
+def download(url, num_retries=2):
+    print('Downloading:', url)
+    try:
+        html = urllib.request.urlopen(url).read()
+    except urllib.error.URLError as e:
+        print('Download error:', e.reason)
+        html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                # 返回 5xx 错误则重试
+                return download(url, num_retries-1)
+    return html
