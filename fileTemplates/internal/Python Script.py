677719e6152fbadc8c99b9ecf88d9ef788e84055
yzh <you_zhihong@aliyun.com>
2018-08-23 12:51:33 +0800

    PC-2018.2 <yzh@DESKTOP-C66BS63 Create ide.general.xml, git.xml, project.default.xml, laf.xml, other.xml, jdk.table.xml, Default.xml, filetypes.xml, Python Script.py, vcs.xml, editor.codeinsight.xml, colors.scheme.xml, github_settings.xml, debugger.xml

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time     : ${DATE} ${TIME}
+# @Author   : YZH
+# @Email    : you_zhihong@aliyun.com
+# @File     : ${NAME}.py
+# @Software : ${PRODUCT_NAME}
