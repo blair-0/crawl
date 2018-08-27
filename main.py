abc9a6bdf6973eb91d5e2ece7438a996d7d6385a
yzh <you_zhihong@aliyun.com>
2018-08-27 13:25:54 +0800

    添加robots.txt检查

--- main.py
+++ main.py
@@ -6,14 +6,16 @@
 # @File     : main.py
 # @Software : PyCharm
 
-import urllib, re
+import urllib
+import re
 import itertools
 from html.parser import HTMLParser
 
 class MyHTMLParser(HTMLParser):
-    def __init__(self):
+    def __init__(self, seed_url):
         HTMLParser.__init__(self)
-        # 存放每个币种和对应的URL
+        # 存放每页每个币种和对应的URL
+        self.seed_url = seed_url
         self.coin_url = {}
         # 是否进入了"id=table"的表格，里面包含币种列表
         self.coin_table_tag = False
@@ -30,10 +32,10 @@ class MyHTMLParser(HTMLParser):
                     self.coin_id = attr[1]
                     self.first_a = True
         if self.first_a and tag == 'a':
-            for att in attrs:
-                if att[0] == 'href':
+            for attr in attrs:
+                if attr[0] == 'href':
                     print('add {}'.format(self.coin_id))
-                    self.coin_url[self.coin_id] = att[1]
+                    self.coin_url[self.coin_id] = urllib.parse.urljoin(self.seed_url, attr[1])
 
     def handle_endtag(self, tag):
         if self.coin_table_tag and tag == 'table':
@@ -48,6 +50,12 @@ def download(url,
                         AppleWebKit/537.36 (KHTML, like Gecko) \
                         Chrome/68.0.3440.106 Safari/537.36',
              num_retries=2):
+    rp = urllib.robotparser.RobotFileParser()
+    rp.set_url(urllib.parse.urljoin(url, 'robots.txt'))
+    rp.read()
+    # robots.txt检查，只显示警告信息，不做实际处理
+    if not rp.can_fetch(user_agent, url):
+        print('Blocked by robots.txt: {}'.format(url))
     print('Downloading:', url)
     request = urllib.request.Request(url)
     request.add_header('User-agent', user_agent)
@@ -72,12 +80,16 @@ def crawl_sitemap(url):
     return html
 
 def iter_url(url):
+    coin_urls = {}
     for page in itertools.count(1):
-        page_url = url + '/list_{}.html'.format(page)
+        page_url = urllib.parse.urljoin(url, '/list_{}.html'.format(page))
         html = download(page_url)
-        parse = MyHTMLParser()
+        parse = MyHTMLParser(url)
         parse.feed(html)
         print(parse.coin_url)
         if not parse.coin_url:
             print(page)
             break
+        coin_urls.update(parse.coin_url)
+    print(coin_urls)
+    print(len(coin_urls))
