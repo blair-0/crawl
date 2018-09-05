f2a1e0c625e77132c226abbbd9cf0c52171e05c4
yzh <you_zhihong@aliyun.com>
2018-09-05 15:38:13 +0800

    完成页面抓取

--- coin.py
+++ coin.py
@@ -14,7 +14,7 @@ import urllib.parse
 class CoinPager(Downloader):
     def __init__(self,
                  url,
-                 type,
+                 type=None,
                  userAgent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
                         AppleWebKit/537.36 (KHTML, like Gecko) \
                         Chrome/68.0.3440.106 Safari/537.36',
@@ -30,13 +30,18 @@ class CoinPager(Downloader):
 class CoinParser(Myparser):
     def __init__(self, html, filter=None):
         Myparser.__init__(self, html, filter)
+        self.api_url = 'https://api.feixiaohao.com'
+        self.url_element = html.url_element
         self.page_type = html.page_type
         self.page_coin_urls = {}
         self.coin_hl_price = {}
         self.coin_sites = []
-        self.api_url = urllib.parse.urljoin('https://api.feixiaohao.com', html.url_element[2])
         self.coin_timeline = {}
         self.coin_markets = {}
+        self.qr_code = {}
+
+    def set_type(self, type):
+        self.page_type = type
 
     def get_coin_url(self):
         if self.page_type == 'coin_list':
@@ -45,8 +50,8 @@ class CoinParser(Myparser):
             # 获取每个coin详情页URL
             for tag in self.soup.select('tbody tr'):
                 self.page_coin_urls[tag['id']] = tag.a['href']
-            print(self.page_coin_urls)
-            print(len(self.page_coin_urls))
+            #print(self.page_coin_urls)
+            #print(len(self.page_coin_urls))
 
     def get_coin_info(self):
         if self.page_type == 'coin_info':
@@ -57,17 +62,20 @@ class CoinParser(Myparser):
                 self.coin_hl_price[tag.contents[0]] = tag.span.string
             # 获取coin 描述页URL
             self.coin_des_url = self.soup.select("div.des a")[0]["href"]
-            print(self.coin_hl_price)
-            print(self.coin_des_url)
             for tag in self.soup.select("div.secondPark li"):
                 if "白皮书" in tag.select("span.tit")[0].text:
                     self.coin_whitepaper_url = tag.select("span.value")[0].text
                 if "网站" in tag.select("span.tit")[0].text:
                     for tag_a in tag.select("span.value")[0].find_all('a'):
-                        self.coin_sites.append(tag_a['href'])
-
-            print(self.coin_whitepaper_url)
-            print(self.coin_sites)
+                        self.coin_sites.append('https:{0}'.format(tag_a['href']))
+                if "区域群" in tag.select("span.tit")[0].text:
+                    for v in tag.select("i.erweima span i"):
+                        self.qr_code[v.text] = urllib.parse.urljoin("https:", tag.i.img['src'])
+            #print(self.qr_code)
+            #print(self.coin_hl_price)
+            #print(self.coin_des_url)
+            #print(self.coin_whitepaper_url)
+            #print(self.coin_sites)
 
 
     def get_coin_des(self):
@@ -75,17 +83,18 @@ class CoinParser(Myparser):
             self.filter = SoupStrainer("div", class_="artBox")
             self.renew_soup(self.filter)
             self.coin_des = self.soup.text
-            print(self.coin_des)
+            #print(self.coin_des)
 
     def get_coin_timeline(self):
         if self.page_type == 'coin_timeline':
-            print(self.api_url)
-            html_timeline = Downloader(self.api_url)
+            self.coin_id = self.url_element[2].split(sep='/')[-2]
+            api_url_timeline = urllib.parse.urljoin(self.api_url, 'coinevent/{0}/'.format(self.coin_id))
+            html_timeline = Downloader(api_url_timeline)
             soup_timeline = Myparser(html_timeline).soup
             for tag in soup_timeline.select("li"):
                 self.coin_timeline[tag.select("div.tit span")[0].text] = (tag.select("div.tit h3")[0].text,
                                                                           tag.select("div.time")[0].text)
-            print(self.coin_timeline)
+            #print(self.coin_timeline)
 
     def get_coin_markets(self):
         if self.page_type == 'coin_markets':
@@ -93,9 +102,11 @@ class CoinParser(Myparser):
             self.renew_soup(self.filter)
             for tag in self.soup.select("tbody tr"):
                 tag_td = tag.select("td")
+                if not tag_td[0].text:
+                    break
                 rank = tag_td[0].text
                 market = tag_td[1].text.strip()
                 trade_pair = tag_td[2].text.strip()
                 price = tag_td[3].text
                 self.coin_markets[rank] = (market, trade_pair, price)
-            print(self.coin_markets)
+            #print(self.coin_markets)
--- start.py
+++ start.py
@@ -5,3 +5,34 @@
 # @Email    : you_zhihong@aliyun.com
 # @File     : start.py
 # @Software : PyCharm
+import itertools
+import urllib.parse
+import coin
+
+def crawl(url):
+    for page in itertools.count(1):
+        page_url = urllib.parse.urljoin(url, '/list_{}.html'.format(page))
+        html = coin.CoinPager(page_url, 'coin_list')
+        c = coin.CoinParser(html)
+        c.get_coin_url()
+        for k,v in c.page_coin_urls.items():
+            coin_html = coin.CoinPager(urllib.parse.urljoin(html.seed_url, v))
+            coin_c = coin.CoinParser(coin_html)
+            coin_c.set_type('coin_info')
+            coin_c.get_coin_info()
+            coin_c.set_type('coin_timeline')
+            coin_c.get_coin_timeline()
+            coin_c.set_type('coin_markets')
+            coin_c.get_coin_markets()
+            coin_des_html = coin.CoinPager(urllib.parse.urljoin(html.seed_url, coin_c.coin_des_url), 'coin_des')
+            coin_des_c = coin.CoinParser(coin_des_html)
+            coin_des_c.get_coin_des()
+            print(coin_c.coin_id)
+            print(coin_c.coin_hl_price)
+            print(coin_des_c.coin_des)
+            print(coin_c.coin_whitepaper_url)
+            print(coin_c.coin_sites)
+            print(coin_c.qr_code)
+            print(coin_c.coin_timeline)
+            print(coin_c.coin_markets)
+            print('-' * 80)
